6841346266f8a454f41c6f9beb795bc2667037ec
Maggie Jenkins <adair.jenkins@gmail.com>
2022-05-09 07:37:44 -0500

    save

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,51 +1,70 @@
 import React from 'react';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
-import { IconButton, Box, Typography, Stack, Paper, Grid, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import { IconButton, Box, Typography, Stack, Paper, Container, Grid, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import { Info } from '@mui/icons-material';
 
 function App() {
   return (
     <>
       <Box sx={{
-        backgroundColor: '#08468c',
+        backgroundColor: '#032142',
         padding: '2em',
+        paddingLeft: '3em',
+        paddingRight: '3em',
         color: 'white',
-        marginBottom: '1em',
       }}>
-        <img src="images/headshot.png" width="260" />
-        <Typography sx={{ fontSize: "2em" }}>Maggie Jenkins</Typography>
-        <IconButton href="https://www.github.com/adairjenkins">
-          <SiGithub size="3em" />
-        </IconButton>
-        <IconButton href="https://www.linkedin.com/in/margaret-jenkins-86391a1a2" >
-        <SiLinkedin size="3em" />
-        </IconButton>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+
+          <Stack direction="row" alignItems="center" spacing={6}>
+            <img src="images/headshot.png" width="200" />
+            <Box>
+              <Typography sx={{ fontSize: "3em" }}>MAGGIE JENKINS</Typography>
+              <Typography sx={{ fontSize: "1.3em" }}>Minneapolis, MN • adair.jenkins@gmail.com</Typography>
+            </Box>
+          </Stack>
+          <Stack direction="row" spacing={3}>
+            <IconButton href="https://www.github.com/adairjenkins">
+              <SiGithub color="white" size="2em" />
+            </IconButton>
+            <IconButton href="https://www.linkedin.com/in/margaret-jenkins-86391a1a2" >
+              <SiLinkedin color="white" size="2em" />
+            </IconButton>
+          </Stack>
+        </Stack>
       </Box>
-      <ImageList gap={10}>
-        <ImageListItem sx={{ margin: 3 }}>
-          <img src="images/exposure-log.png" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-        <ImageListItem>
-          <img src="images/movie-sagas.png" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-        <ImageListItem>
-          <img src="images/react-gallery.gif" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-        <ImageListItem>
-          <img src="images/to-do-list.png" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-        <ImageListItem>
-          <img src="images/jquery-calculator.png" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-        <ImageListItem>
-          <img src="images/salary-calculator.png" />
-          <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
-        </ImageListItem>
-      </ImageList>
+      <Box sx={{padding:'2em', backgroundColor:'#3D3D3D'}}>
+        <ImageList gap={20}>
+          <ImageListItem >
+            <img src="images/exposure-log-sq.png" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below"
+              actionIcon={
+                <IconButton>
+                  <Info />
+                </IconButton>}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="images/movie-sagas-sq.png" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below"/>
+          </ImageListItem>
+          <ImageListItem>
+            <img src="images/react-gallery.gif" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="images/to-do-list.png" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="images/jquery-calculator-sq.png" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="images/salary-calculator-sq.png" />
+            <ImageListItemBar title="Solo Project: Exposure Log" position="below" />
+          </ImageListItem>
+        </ImageList>
+      </Box>
 
       {/* <Grid container>
         <Grid item>
